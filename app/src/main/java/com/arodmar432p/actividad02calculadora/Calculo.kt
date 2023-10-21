95d27ff9a77ab38fdf07fd8a7cc9713f2ccfb667
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-10-21 13:53:38 +0200

    new functions added.
    -fun BotonLimpiarC has been added
    
    -setPuntoClicked has been added.
    
    -improved comments.

--- app/src/main/java/com/arodmar432p/actividad02calculadora/Calculo.kt
+++ app/src/main/java/com/arodmar432p/actividad02calculadora/Calculo.kt
@@ -9,11 +9,11 @@ class Calculo(private val context: Context) {
     var operacion: String = ""
     var resultado: Double = 0.0
     var fase: Int = 0 // Controla la fase en la que está mi calculadora (de 0 a 3).
+    var operacionRecienIntroducida = false
 
     /**
      * Los siguientes funciones se encargan de crear la lógica operacional de la calculadora.
      */
-
     fun sumar() {
         resultado = num1.toDouble() + num2.toDouble()
     }
@@ -59,7 +59,8 @@ class Calculo(private val context: Context) {
         }
         num2 = ""
         operacion = ""
-        fase = 3 // Se ha realizado una operación, si se presiona un número la calculadora se resetea.
+        fase =
+            3 // Se ha realizado una operación, si se presiona un número la calculadora se resetea.
     }
 
     /**
@@ -83,6 +84,9 @@ class Calculo(private val context: Context) {
      */
     fun setOperaciones(oper: String) {
         if (num1.isNotEmpty()) {
+            if (operacion.isNotEmpty() && num2.isNotEmpty()) {
+                calcular()
+            }
             operacion = oper
             fase = 2 // Esperando a num2
         }
@@ -98,5 +102,34 @@ class Calculo(private val context: Context) {
         operacion = ""
         resultado = 0.0
         fase = 0
+        operacionRecienIntroducida = false
+    }
+
+    fun setPuntoClicked() {
+        if (fase == 0 && !num1.contains(".")){
+            num1 += "."
+        } else if (fase == 2 && !num2.contains(".")) {
+            num2 += "."
+        }
+    }
+
+    /**
+     * Método `botonLimpiarC` que se llama cuando se pulsa el botón C.
+     * Borra los dígitos uno a uno del número actual.
+     * Si no hay más dígitos para borrar, borra la operación.
+     * Si no hay operación para borrar, borra los dígitos del primer número.
+     * Si no hay nada que eliminar, se emite un mensaje tipo toast "No hay nada más que borrar".
+     */
+    fun botonLimpiarC() {
+        if (num2.isNotEmpty()) {
+            num2 = num2.dropLast(1)
+        } else if (operacion.isNotEmpty()) {
+            operacion = ""
+            fase = 0 // Esperando a num1
+        } else if (num1.isNotEmpty()) {
+            num1 = num1.dropLast(1)
+        } else {
+            mostrarToast("No hay nada más que borrar.")
+        }
     }
 }
